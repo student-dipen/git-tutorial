5438dc11f3986ef7d6ff34c6e356f09786b6f586
Student Dipen <baniyadipen9856@gmail.com>
2024-05-28 10:13:01 -1200

    first commit

---
+++ practice2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+bool isPrime(int n)
+{
+
+    if (n < 2)
+        return false;
+
+    else if (n == 2 || n == 3)
+        return true;
+
+    for (int i = 2; i * i < n; i++)
+    {
+        if (n % i == 0)
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+
+    cout << "Enter number to be checked:";
+    int num;
+    cin >> num;
+
+    cout << isPrime(num);
+}
